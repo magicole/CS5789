dac805ecc4602946221f786f8bf7ecb520afed5b
Cole Mortensen <colemortensen@gmail.com>
2019-02-28 15:05:21 -0700

    initial commit with code structure

---
+++ turning_heads/turning_heads.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+  
+  // set up random number generators
+  // pin set up for servo board
+  // pin set up for ping sensor
+  // initialize connection to servo board
+  // put servos in their starting positions
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // make sensor reading
+  // see if it is within the limit
+  // if it is then turn the heads
+  // else increment head position by one
+  // if heads are looking at eachother
+  // if they are then turn them away
+}
